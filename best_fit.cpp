f27e6c8a4cf88b6ec7212efd6fc9a8eec3ebf556
dayuebai <dayueb@uci.edu>
2019-05-14 00:17:07 -0700

    Working on best fit

--- best_fit.cpp
+++ best_fit.cpp
@@ -1,10 +1,14 @@
 #include "project2.h"
 #include <iostream>
 
+using namespace std;
+
 
 // Implementation of best fit algorithm
-void best_fit(const std::vector<double>& items, std::vector<int>& assignment, std::vector<double>& free_space);
+void best_fit(const vector<double>& items, vector<int>& assignment, vector<double>& free_space) {
+    
+}
 
 
 // Implementation best fit decreasing algorithm
-void best_fit_decreasing(const std::vector<double>& items, std::vector<int>& assignment, std::vector<double>& free_space);
+void best_fit_decreasing(const vector<double>& items, vector<int>& assignment, vector<double>& free_space);
